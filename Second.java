0ec2aad66dc572622dc1a061d27fc471501de49b
Vimal <vimalsingh963vs@gmail.com>
2026-04-04 23:52:07 +0530

    Second File Add

---
+++ Second.java
@@ -0,0 +1,23 @@
+
+import java.util.Date;
+public class Second {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is Java Program");
+		System.out.println("Output from Java Program");
+		System.out.println(new Date());
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
